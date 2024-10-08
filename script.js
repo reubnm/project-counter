0ebc2252e86c387577e91bef2db0e02ab241232f
Reuben Maharaj <rmaharaj365@gmail.com>
2024-10-08 15:22:42 +1300

    step 6 complete

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const element = document.getElementById("button")
 element.addEventListener('click', function() {
     alert("on click handler")
 })
+
+let label = document.getElementById('text')
+let count = 0
+label.textContent = `clicked ${count} times`
